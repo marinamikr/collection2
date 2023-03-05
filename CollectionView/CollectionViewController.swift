045e3e64af9f2c4c8c6fbb11831ec9610f20f5a2
Marina <marinahara0223@yahoo.co.jp>
2023-03-06 02:27:05 +0900

    コード汚いごめん

--- CollectionView/CollectionViewController.swift
+++ CollectionView/CollectionViewController.swift
@@ -42,22 +42,15 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         return cell
     }
     
-    
-    // Cell が選択された場合
-    func collectionView(_ collectionView: UICollectionView,
-                        didSelectItemAt indexPath: IndexPath) {
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("aa")
-        // [indexPath.row] から画像名を探し、UImage を設定
         selectedTitle  = titles[indexPath.item]
         if selectedTitle != nil {
-            // SubViewController へ遷移するために Segue を呼び出す
             performSegue(withIdentifier: "toView",sender: nil)
         }
         
     }
-    
-    // Segue 準備
-    override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
+        override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
         if (segue.identifier == "toView") {
             let subVC: SelectedViewController = (segue.destination as? SelectedViewController)!
             // SubViewController のselectedImgに選択された画像を設定する
